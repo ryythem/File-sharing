f7e946387cdd3ae918925776d2837f36e73acaaa
RythemGoyal <imrythem16@gmail.com>
2025-03-14 14:07:48 +0530

    .env added

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+let serviceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+} else {
+  serviceAccount = require("./serviceAccountKey.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
